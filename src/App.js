7d356a8cebab53516e23510a3a09985f8bb14997
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2024-04-02 17:37:27 +0200

    changed nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
-
-import SharedLayout from "./components/sharedLayout/SharedLayout";
+import {  Header, Link,Nav } from "./components/sharedLayout/SharedLayout.styled";
+import Container from "./components/containter/Container";
+// import SharedLayout from "./components/sharedLayout/SharedLayout";
 import Home from "./pages/home/Home";
 import Portfolio from "./pages/portfolio/Portfolio";
 import Contacts from "./pages/contacts/Contacts";
@@ -12,13 +13,31 @@ import Info from "./pages/info/Info";
 export default function App(){
     return(
         <Fragment>
+            <Header>
+            <Container>
+                <Nav>
+                    <Link to="/"end>
+                        Hallo
+                    </Link>
+                    <Link to="/info">
+                        Info
+                    </Link>
+                    <Link to="/portfolio">
+                        Beispiele
+                    </Link>
+                    <Link to="/contacts">
+                        Contacts
+                    </Link>
+                </Nav>
+            </Container>    
+            </Header>
                 <Routes>
-                <Route path="/" element={<SharedLayout/>}>
+                
                     <Route index element={<Home/>}/>
                     <Route path="/info" element={<Info/>}/>
                     <Route path="/portfolio" element={<Portfolio/>}/>
                     <Route path="/contacts" element={<Contacts/>}/>
-                </Route>    
+                  
             </Routes>
         </Fragment>
     )
