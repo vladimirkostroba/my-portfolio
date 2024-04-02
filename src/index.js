ab71c524965d5110a2107a632e77fe8f2b25f428
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2024-04-02 17:42:17 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/my-portfolio">
+  <React.StrictMode>
+    <BrowserRouter basename="/my-portfolio">
     <App />
   </BrowserRouter>
+  </React.StrictMode>
 );
